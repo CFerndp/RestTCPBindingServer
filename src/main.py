93ec477ef03a8789513cc709bba800355563e063
Cristian Fernandez <cristian.fernandez.del.pozo@gmail.com>
2024-07-12 18:26:40 +0200

    Moved to LSL

--- src/main.py
+++ src/main.py
@@ -5,20 +5,15 @@ from typing import Union
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-import socket
-
-TCP_HOST = "127.0.0.1"
-TCP_PORT = 1234
+from pylsl import StreamInfo, StreamOutlet
 
 
 app = FastAPI()
-
 mainRoute = "/enobio_binding"
 
 origins = [
     "*"
 ]
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,  # Allows the origins listed in the `origins` list
@@ -30,29 +25,33 @@ app.add_middleware(
 
 Logger.basicConfig(level=Logger.INFO)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((TCP_HOST, TCP_PORT))
-    
+MARKER_NAME = "GNB_P300_Marker"
 
-    @app.get("/")
-    def read_root():
-        return {"Hello": "World"}
+print ("Creating a new marker stream info...\n")
+info = StreamInfo(MARKER_NAME,'Markers',1,0,'int32','myuniquesourceid23443')
+
+print("Opening an outlet...\n")
+outlet =StreamOutlet(info)
+
+print("Ready to send data...\n")
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
 
-    @app.post(f'{mainRoute}/start_experiment')
-    def start_experiment():
-        Logger.info("Starting experiment")
-        s.sendall(b"start_experiment")
-        return {"status": "ok"}
+@app.post(f'{mainRoute}/start_experiment')
+def start_experiment():
+    Logger.info("Starting experiment")
+    return {"status": "ok"}
 
-    @app.post(f'{mainRoute}/record_timestamp')
-    def record_timestamp():
-        s.sendall(b"RECORD MTF")
-        Logger.info("Record Timestamp at " + str(time.time()))
-        return {"status": "ok"}
+@app.post(mainRoute+"/record_timestamp/{record_id}")
+def record_timestamp(record_id: int):
+    Logger.info(f'Recording timestamp {record_id} at {time.time()}')
+    outlet.push_sample(record_id) 
+    return {"status": "ok"}
 
-    @app.post(f'{mainRoute}/stop')
-    def stop():
-        s.sendall(b"STOP")
-        Logger.info("Stopping experiment at " + str(time.time()))
-        return {"status": "ok"}
+@app.post(f'{mainRoute}/stop')
+def stop():
+    Logger.info("Stopping experiment at " + str(time.time()))
+    return {"status": "ok"}
